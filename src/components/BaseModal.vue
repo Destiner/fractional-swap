7ad934b4a227b73569ccf5231ae21f9dc115b8c7
Timur Badretdinov <destinerx@gmail.com>
2022-04-02 04:33:00 +0400

    Add BaseModal component

---
+++ src/components/BaseModal.vue
@@ -0,0 +1,84 @@
+<template>
+  <Teleport to="body">
+    <div
+      v-if="open"
+      class="wrapper"
+    >
+      <div
+        class="blocker"
+        @click="close"
+      />
+      <div class="view">
+        <div class="header">
+          {{ title }}
+          <IconClose
+            class="icon"
+            @click="close"
+          />
+        </div>
+        <slot />
+      </div>
+    </div>
+  </Teleport>
+</template>
+
+<script
+  setup
+  lang="ts"
+>
+import IconClose from './icons/IconClose.vue';
+
+defineProps({
+  open: {
+    type: Boolean,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+});
+
+const emit = defineEmits(['close']);
+
+function close() {
+  emit('close');
+}
+</script>
+
+<style scoped>
+.blocker {
+  content: ' ';
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: #00000080;
+}
+
+.view {
+  position: fixed;
+  top: 50vh;
+  left: 50vw;
+  min-width: 360px;
+  max-width: 100%;
+  padding: 8px;
+  transform: translate(-50%, -50%);
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  background: #fff;
+}
+
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 24px;
+}
+
+.icon {
+  width: 20px;
+  height: 20px;
+}
+</style>
