ae956fec6d55f666b67ab69cddafcad9e915ca1a
Timur Badretdinov <destinerx@gmail.com>
2022-04-02 08:01:59 +0400

    Add WalletChip component

---
+++ src/components/WalletChip.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="chip">
+    <div class="icon">
+      <WalletIcon
+        :address="address"
+        :size="16"
+      />
+    </div>
+    <div class="address">
+      {{ label }}
+    </div>
+  </div>
+</template>
+
+<script
+  setup
+  lang="ts"
+>
+import { computed } from 'vue';
+
+import WalletIcon from './WalletIcon.vue';
+
+const props = defineProps({
+  address: {
+    type: String,
+    required: true,
+  },
+});
+
+const label = computed(() => {
+  return props.address.substring(0, 8);
+});
+</script>
+
+<style scoped>
+.chip {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+}
+
+.icon {
+  width: 16px;
+  height: 16px;
+  border: 1px solid #333;
+  border-radius: 50%;
+  background: white;
+}
+</style>
