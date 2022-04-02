f6b3942b7e1c70fd30421712cc6f2904b82449fb
Timur Badretdinov <destinerx@gmail.com>
2022-04-02 08:49:52 +0400

    Update wallet store

--- src/stores/wallet.ts
+++ src/stores/wallet.ts
@@ -6,6 +6,7 @@ type Connector = 'metamask';
 
 const useStore = defineStore('counter', () => {
   const connector = ref<Connector | null>(null);
+  const address = ref<string | null>(null);
   const balance = ref<BigNumber | null>(null);
 
   const isConnected = computed(() => connector.value !== null);
@@ -14,15 +15,30 @@ const useStore = defineStore('counter', () => {
     connector.value = newConnector;
   }
 
+  function setAddress(newAddress: string) {
+    address.value = newAddress;
+  }
+
   function setBalance(newBalance: BigNumber) {
     balance.value = newBalance;
   }
 
   function disconnect() {
     connector.value = null;
+    address.value = null;
+    balance.value = null;
   }
 
-  return { connector, balance, isConnected, connect, setBalance, disconnect };
+  return {
+    connector,
+    address,
+    balance,
+    isConnected,
+    connect,
+    setAddress,
+    setBalance,
+    disconnect,
+  };
 });
 
 export type { Connector };
