4caea6bfed13dfb798722a9641c3667cfea171bb
Timur Badretdinov <destinerx@gmail.com>
2022-04-05 05:17:46 +0400

    Add SwapInput to home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,19 +1,39 @@
 <template>
-  <div />
+  <div class="page">
+    <SwapInput
+      :amount-in="amountIn"
+      :asset-out="assetOut"
+      @amount-in-change="handleAmountChange"
+      @asset-out-change="handleAssetChange"
+    />
+  </div>
 </template>
 
 <script
   setup
   lang="ts"
 >
-import { onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 
+import SwapInput from '@/components/SwapInput.vue';
 import { FractionalService } from '@/services';
 import { useNiftyStore } from '@/stores';
 
 const fractionalService = new FractionalService();
 
 const niftyStore = useNiftyStore();
+
+const amountIn = ref('0');
+
+function handleAmountChange(newAmount: string) {
+  amountIn.value = newAmount;
+}
+
+const assetOut = ref('0x8cA9a0fbd8DB501F013F2e9e33a1B9dC129A48E0');
+
+function handleAssetChange(newAsset: string) {
+  assetOut.value = newAsset;
+}
 
 onMounted(async () => {
   const collections = [
@@ -29,3 +49,12 @@ onMounted(async () => {
   }
 });
 </script>
+
+<style scoped>
+.page {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 600px;
+}
+</style>
